613ba7eaaa32f8d2dd2f63b11959c0c6146f9904
Christian Lewis <ctjl96@gmail.com>
2020-06-12 01:01:41 -0500

    BULK AUTOMATIC COMMIT

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let JSDOM = require('jsdom'),
-    DOM = new JSDOM.JSDOM(`<html><body></body></html>`);
+    DOM = new JSDOM.JSDOM(`<html><body></body></html>`, {
+        url: 'https://localhost'
+    });
 
 global.window = DOM.window,
 global.document = window.document;
